d2cbbe3d8944a66cb7c090267cc2660005956a74
Oyekola Toheeb <toheeb.olawale.to23@gmail.com>
2023-06-06 11:08:14 +0100

    progress towards installing reactPHP, lookin forward to working with it

---
+++ php/async-php/index.php
@@ -0,0 +1,27 @@
+<?php
+
+use React\EventLoop\Loop;
+use React\EventLoop\LoopInterface;
+
+require __DIR__ . '/vendor/autoload.php';
+
+class Greeter
+{
+    private $loop;
+
+    public function __construct(LoopInterface $loop)
+    {
+        $this->loop = $loop;
+    }
+
+    public function greet(string $name)
+    {
+        $this->loop->addTimer(1.0, function () use ($name) {
+            echo 'Hello ' . $name . '!' . PHP_EOL;
+        });
+    }
+}
+
+$greeter = new Greeter(Loop::get());
+$greeter->greet('Alice');
+$greeter->greet('Bob');
